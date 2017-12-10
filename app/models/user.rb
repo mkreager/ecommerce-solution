f6ee9cf990da729df501bd7752b7f97af5b3da4d
Mike Kreager <mkreager@users.noreply.github.com>
2017-12-10 10:35:41 -0500

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,38 +1,38 @@
 class User < ApplicationRecord
-	has_many :albums
+  has_many :albums
   validates :email, uniqueness: true, presence: true
-	before_save :format_email
+  before_save :format_email
 
-	def format_email
-	  	self.email = self.email.delete(' ').downcase
-	end
+  def format_email
+    self.email = self.email.delete(' ').downcase
+  end
 
-	def generate_login_token
-	    self.login_token = generate_token
-	    self.token_generated_at = Time.now.utc
-	    save!
-	end
+  def generate_login_token
+    self.login_token = generate_token
+    self.token_generated_at = Time.now.utc
+    save!
+  end
 	
-	def login_link
-	    ActionMailer::Base.default_url_options[:host] + "/auth?token=#{self.login_token}"
-	end
+  def login_link
+    ActionMailer::Base.default_url_options[:host] + "/auth?token=#{self.login_token}"
+  end
 	
-	def login_token_expired?
-	    Time.now.utc > (self.token_generated_at + token_validity)
-	end
+  def login_token_expired?
+    Time.now.utc > (self.token_generated_at + token_validity)
+  end
 	
-	def expire_token!
-	    self.login_token = nil
-	    save!
-	end
+  def expire_token!
+    self.login_token = nil
+    save!
+  end
 	
-	private
+private
 	
-	def generate_token
-	    SecureRandom.hex(10)
-	end
+  def generate_token
+    SecureRandom.hex(10)
+  end
 	
-	def token_validity
-	    2.hours
-	end
+  def token_validity
+    2.hours
+  end
 end
